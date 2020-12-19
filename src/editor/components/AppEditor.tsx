25f05dc5cef47c0dff37f335b8f557646bc686e5
Alex Diaz <alexdiaz.jdz@gmail.com>
2020-12-19 21:31:39 +0100

    reimplement basic cursor

--- src/editor/components/AppEditor.tsx
+++ src/editor/components/AppEditor.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent, MutableRefObject } from 'react'
-import styled from 'styled-components'
+import React, { FunctionComponent, MutableRefObject, MouseEvent, useState } from 'react'
+import styled, { css, keyframes } from 'styled-components'
 
 type Editor = {
   lines: string[]
@@ -7,16 +7,39 @@ type Editor = {
 }
 
 const AppEditor: FunctionComponent<Editor> = ({ lines, inputHandlerRef }) => {
-  const handleClick = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 })
+
+  const handleClick = (evt: MouseEvent) => {
     inputHandlerRef.current.focus()
+
+    const character = evt.target as HTMLSpanElement
+
+    if (character.parentElement.hasAttribute('data-line')) {
+      const x = Number(character.getAttribute('data-char-index'))
+      const y = Number(character.getAttribute('data-line-number'))
+      setCoords({ x, y })
+    }
   }
 
   return (
-    <Container>
-      {lines.map((line, number) => (
-        <Line key={number} onClick={handleClick}>
-          <LineNumber data-number>{number}</LineNumber>
-          <LineText data-line>{line}</LineText>
+    <Container onClick={handleClick}>
+      {coords.x}:{coords.y}
+      {lines.map((line, lineNumber) => (
+        <Line key={lineNumber}>
+          <LineNumber data-number>{lineNumber}</LineNumber>
+          <LineText data-line>
+            {[...line].map((char, charIndex) => (
+              <Character
+                key={charIndex}
+                onClick={handleClick}
+                data-char-index={charIndex}
+                data-line-number={lineNumber}
+                data-active={charIndex === coords.x && lineNumber === coords.y}
+              >
+                {char}
+              </Character>
+            ))}
+          </LineText>
         </Line>
       ))}
     </Container>
@@ -51,4 +74,18 @@ const LineNumber = styled.div`
 
 const LineText = styled.div`
   max-width: 80ch;
+`
+
+const animation = keyframes`
+  50% {
+    box-shadow: 2px 0px 0px 0px #555;
+  }
+`
+
+const Character = styled('span')<{ 'data-active': boolean }>`
+  ${({ 'data-active': active }) =>
+    active &&
+    css`
+      animation: ${animation} 1s step-start infinite;
+    `}
 `
